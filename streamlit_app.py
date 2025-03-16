a33d890c5a2678e61f1f10ff0ea0df72654e205e
HarikaBotla28 <hbotla@charlotte.edu>
2025-03-16 01:53:14 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,10 +30,8 @@ st.dataframe(sales_by_month)
 st.line_chart(sales_by_month, y="Sales")
 
 st.write("## Your additions")
-# (1) Dropdown for Category Selection
-categories = df["Category"].unique()
-selected_category = st.selectbox("Select a Category:", categories)
-
-# (2) Multi-select for Sub-Category based on selected Category
-sub_categories = df[df["Category"] == selected_category]["Sub-Category"].unique()
-selected_sub_categories = st.multiselect("Select Sub-Categories:", sub_categories)
+st.write("### (1) add a drop down for Category (https://docs.streamlit.io/library/api-reference/widgets/st.selectbox)")
+st.write("### (2) add a multi-select for Sub_Category *in the selected Category (1)* (https://docs.streamlit.io/library/api-reference/widgets/st.multiselect)")
+st.write("### (3) show a line chart of sales for the selected items in (2)")
+st.write("### (4) show three metrics (https://docs.streamlit.io/library/api-reference/data/st.metric) for the selected items in (2): total sales, total profit, and overall profit margin (%)")
+st.write("### (5) use the delta option in the overall profit margin metric to show the difference between the overall average profit margin (all products across all categories)")
